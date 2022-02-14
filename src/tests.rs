7daa03984d957c61287229cc76ecf69d826d40ae
philip <philip_huels@googlemail.com>
2022-02-14 18:31:32 +0100

    env_logger crate added & additional tests added

--- src/tests.rs
+++ src/tests.rs
@@ -1,10 +1,12 @@
 #[cfg(test)]
 use super::*;
+use rocket::http::ContentType;
 use rocket::http::Status;
 use rocket::local::blocking::Client;
+use std::path::Path;
 
 #[test]
-fn check_root_ok() {
+fn ok_root_ok() {
     let client = Client::tracked(rocket()).expect("Invalid rocket instance");
     let response = client.get("/").dispatch();
 
@@ -12,7 +14,8 @@ fn check_root_ok() {
 }
 
 #[test]
-fn check_set_dosage_without_dosage_ok() {
+fn ok_set_dosage() {
+    // More like an integration test because this will only work with a running postgres db
     let client = Client::tracked(rocket()).expect("Invalid rocket instance");
     let response = client.post("/set_dosage").dispatch();
 
@@ -20,9 +23,41 @@ fn check_set_dosage_without_dosage_ok() {
 }
 
 #[test]
-fn check_set_dosage() {
+fn ok_set_dosage_without_dosage_ok() {
     let client = Client::tracked(rocket()).expect("Invalid rocket instance");
     let response = client.post("/set_dosage").dispatch();
+    let response_body = response.into_string();
 
-    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(
+        response_body,
+        Some("No Dosage set. Doing nothing :(".to_string())
+    );
+}
+
+#[test]
+fn error_set_dosage() {
+    let client = Client::tracked(rocket()).expect("Invalid rocket instance");
+    let response = client
+        .post("/set_dosage")
+        .header(ContentType::Form)
+        .body("dosage=Foo")
+        .dispatch();
+
+    assert_eq!(response.status(), Status::BadRequest);
+}
+
+// #[test]
+// fn user_signup_test() {
+//     let client = Client::tracked(rocket()).expect("Invalid rocket instance");
+//     let response = client.post("/signup_user").dispatch();
+
+//     assert_eq!(response.status(), Status::Ok);
+// }
+
+#[test]
+fn logging_test() {
+    let client = Client::tracked(rocket()).expect("Invalid rocket instance");
+    client.get("/").dispatch();
+
+    assert!(Path::new("simple_log.log").exists());
 }
